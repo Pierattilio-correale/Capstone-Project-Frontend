26b58f5fdd3a783882f41270398edfd4b176a4aa
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-07-01 12:05:03 +0200

    add animation and button on navbar

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -1,3 +1,4 @@
+import { Row, Col, Form, Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -37,6 +38,57 @@ function MyNavbar() {
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
+          <Form className="d-flex ms-auto">
+            <Form.Control
+              type="text"
+              placeholder="Cerca il tuo libro.."
+              className="me-2"
+            />
+            <Button
+              type="submit"
+              className="buttonanimation"
+              style={{
+                background: "linear-gradient(90deg, #7f00ff, #00bfff)",
+                border: "none",
+                color: "white",
+                padding: "0.5rem 1rem",
+                borderRadius: "0.375rem", // ~rounded-md
+                boxShadow: "0 2px 6px rgba(0, 0, 0, 0.2)",
+              }}
+            >
+              Submit
+            </Button>
+            <div className="mx-2">
+              <Button
+                className="buttonanimation"
+                style={{
+                  background: "linear-gradient(90deg, #7f00ff, #00bfff)",
+                  border: "none",
+                  color: "white",
+                  padding: "0.5rem 1rem",
+                  borderRadius: "0.375rem",
+                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.2)",
+                }}
+              >
+                Login
+              </Button>
+            </div>
+            <div className="mx-2">
+              <Button
+                className="buttonanimation"
+                style={{
+                  background: "linear-gradient(90deg, #7f00ff, #00bfff)",
+                  border: "none",
+                  color: "white",
+                  padding: "0.5rem 1rem",
+                  borderRadius: "0.375rem",
+                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.2)",
+                }}
+              >
+                Registrazione
+              </Button>
+            </div>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
